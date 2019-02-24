1ec2fa3bd63569ec09416569873a2e60c1f6caa9
Iosif <iosiflivadaru@yahoo.com>
2019-02-23 17:36:13 -0800

    Submission

---
+++ inc/footer.inc.php
@@ -0,0 +1,4 @@
+<footer class="text-center my-4 text-muted">&copy; 2019 TempConv | Iosif Livadaru </footer>
+</div>
+</body>
+</html>
--- lab8.php
+++ lab8.php
@@ -1,73 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-	<title>Temperature Converter</title>
-
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<script src="js/jquery-3.3.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-
-</head>
-
-<body>
 <?php 
+	include 'inc/header.inc.php';
 	include 'inc/functions.inc.php';
+	include 'inc/nav.inc.php'
 ?>
-<div class="container">
-	<h1 class="text-center my-4">Temperature Converter</h1>
 
-	<div class="jumbotron p-4 alert-info text-dark mb-4">
-		<h1 class="display-5">Hello there!</h1>
-		<p class="lead">This temperature converter can convert your chosen temperature to Celsius, Fahrenheit or Kelvin.</p>
-		<hr class="my-4">
-		<p>Below the convertor you can find more information about the temperature measurements used for this convertor.</p>
-	</div>
+<h1 class="text-center my-4">Temperature Converter</h1>
 
-	<div class="row mb-4">
-		<div class="col  d-none d-md-block"></div>
-		<div class="col-md-auto">
-			
-			<div class="card">
-				<div class="card-header alert-info text-dark">
-					<h4 class="m-0">Convertor</h4>	
-				</div>
-				
-				<form class="p-3" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">					
-					<div class="form-group m-0">
-						<label class="d-block" for="temp">Temperature:</label>					
-						<input class="form-control w-auto d-inline-block" type="text" value="<?php if (isset($_POST['originaltemp'])) echo $_POST['originaltemp'];?>" name="originaltemp" size="7" maxlength="7" id="temp">
-						<select class="form-control w-auto d-inline-block mb-3" name="originalunit">
-							<option value="--Select--"<?php if($originalUnit == "--Select--") echo ' selected="selected"';?> hidden>--Select--</option>
-							<option value="celsius"<?php if($originalUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
-							<option value="fahrenheit"<?php if($originalUnit == "Fahrenheit") echo ' selected="selected"';?>>Fahrenheit</option>
-							<option value="kelvin"<?php if($originalUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
-						</select>
-					</div>
+<div class="jumbotron p-4 alert-info text-dark mb-4">
+	<h1 class="display-5">Hello there!</h1>
+	<p class="lead">This temperature converter can convert your chosen temperature to Celsius, Fahrenheit or Kelvin.</p>
+	<hr class="my-4">
+	<p>Below the convertor you can find more information about the temperature measurements used for this convertor.</p>
+</div>
 
-					<div class="form-group m-0">
-						<label class="d-block" for="convertedtemp">Converted Temperature:</label>			
-						<input class="form-control w-auto d-inline-block bg-white" type="text" value="<?php if (isset($_POST['originaltemp'])) echo round($convertedTemp, 1);?>" 
-						name="convertedtemp" size="7" maxlength="7" id="convertedtemp" readonly>
-						<select class="form-control w-auto d-inline-block mb-3" name="conversionunit">
-							<option value="--Select--"<?php if($conversionUnit == "--Select--") echo ' selected="selected"';?> hidden>--Select--</option>
-							<option value="celsius"<?php if($conversionUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
-							<option value="fahrenheit"<?php if($conversionUnit == "fahrenheit") echo ' selected="selected"';?>>Fahrenheit</option>
-							<option value="kelvin"<?php if($conversionUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
-						</select>
-					</div>
-
-					<input class="btn btn-primary w-100" type="submit" value="Convert"/>   
-				</form>
+<div class="row mb-4">
+	<div class="col  d-none d-md-block"></div>
+	<div class="col-md-auto">
+		
+		<div class="card">
+			<div class="card-header alert-info text-dark">
+				<h4 class="m-0">Convertor</h4>	
 			</div>
+			
+			<form class="p-3" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">					
+				<div class="form-group m-0">
+					<label class="d-block" for="temp">Temperature:</label>					
+					<input class="form-control w-auto d-inline-block" type="text" value="<?php if (isset($_POST['originaltemp'])) echo $_POST['originaltemp'];?>" name="originaltemp" size="7" maxlength="7" id="temp">
+					<select class="form-control w-auto d-inline-block mb-3" name="originalunit">
+						<option value="--Select--"<?php if($originalUnit == "--Select--") echo ' selected="selected"';?> hidden>--Select--</option>
+						<option value="celsius"<?php if($originalUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
+						<option value="fahrenheit"<?php if($originalUnit == "Fahrenheit") echo ' selected="selected"';?>>Fahrenheit</option>
+						<option value="kelvin"<?php if($originalUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
+					</select>
+				</div>
+
+				<div class="form-group m-0">
+					<label class="d-block" for="convertedtemp">Converted Temperature:</label>			
+					<input class="form-control w-auto d-inline-block bg-white" type="text" value="<?php if (isset($_POST['originaltemp'])) echo round($convertedTemp, 1);?>" 
+					name="convertedtemp" size="7" maxlength="7" id="convertedtemp" readonly>
+					<select class="form-control w-auto d-inline-block mb-3" name="conversionunit">
+						<option value="--Select--"<?php if($conversionUnit == "--Select--") echo ' selected="selected"';?> hidden>--Select--</option>
+						<option value="celsius"<?php if($conversionUnit == "celsius") echo ' selected="selected"';?>>Celsius</option>
+						<option value="fahrenheit"<?php if($conversionUnit == "fahrenheit") echo ' selected="selected"';?>>Fahrenheit</option>
+						<option value="kelvin"<?php if($conversionUnit == "kelvin") echo ' selected="selected"';?>>Kelvin</option>
+					</select>
+				</div>
+
+				<input class="btn btn-primary w-100" type="submit" value="Convert"/>   
+			</form>
 		</div>
-		<div class="col d-none d-md-block"></div>
-	</div> <!-- end row -->
+	</div>
+	<div class="col d-none d-md-block"></div>
+</div> <!-- end row -->
 
 <div class="row">
 
@@ -142,10 +126,6 @@
 
 </div>
 
-<footer class="text-center my-4 text-muted">&copy; 2019 TempConv | Iosif Livadaru </footer>
-</div>
-</body>
-</html>
-
-		
-			
+<?php 
+	include 'inc/footer.inc.php';
+?>			
